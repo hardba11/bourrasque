3d545e0e4a3e971910e8b80a02d3d868991e6c5e
hardba11 <root@yoshi.localdomain>
2017-10-14 16:22:25 +0200

    ISSUE #18 : create a tool to record a mach-loop

---
+++ tools/trace-loop/csv2xml-loop.pl
@@ -0,0 +1,178 @@
+#!/usr/bin/perl -W
+
+# author : hardball
+# version : v20171014-01
+# description :
+#   csv loop to xml scenario
+
+use Term::ANSIColor ;
+
+#===============================================================================
+#                                                                     CONSTANTES
+
+# filepath
+my $FILENAME_IN_CSV         = 'trace-loop-INPUT.csv' ;
+my $FILENAME_INC_HEADER_XML = 'loop-HEADER.inc.xml' ;
+my $FILENAME_INC_FOOTER_XML = 'loop-FOOTER.inc.xml' ;
+my $FILENAME_OUT_XML        = 'HOMEMADE-timed-loop.xml' ;
+
+my $TPL_ENTRANCE = '
+    <entry>
+      <name>Line%02d</name>
+      <model type="string">AI/HOMEMADE-timed-loop/lineupmarker.xml</model>
+      <latitude type="double">%s</latitude>
+      <longitude type="double">%s</longitude>
+      <altitude type="double">%s</altitude>
+      <heading type="double">%s</heading>
+    </entry>
+' ;
+
+my $TPL_START1 = '
+    <entry>
+      <name>Start-Marker</name>
+      <model type="string">AI/HOMEMADE-timed-loop/startmarker.xml</model>
+      <latitude type="double">%s</latitude>
+      <longitude type="double">%s</longitude>
+      <altitude type="double">%s</altitude>
+      <heading type="double">%s</heading>
+    </entry>' ;
+my $TPL_START2 = '
+    <entry>
+      <callsign>Start</callsign>
+      <name>Start</name>
+      <model type="string">AI/HOMEMADE-timed-loop/targetmarker-inactive.xml</model>
+      <latitude type="double">%s</latitude>
+      <longitude type="double">%s</longitude>
+      <altitude type="double">%s</altitude>
+      <heading type="double">%s</heading>
+    </entry>
+' ;
+
+my $TPL_LOOP = '
+    <entry>
+      <name>%d</name>
+      <model type="string">AI/HOMEMADE-timed-loop/targetmarker.xml</model>
+      <latitude type="double">%s</latitude>
+      <longitude type="double">%s</longitude>
+      <altitude type="double">%s</altitude>
+      <heading type="double">%s</heading>
+    </entry>
+' ;
+
+my $TPL_FINISH1 = '
+    <entry>
+      <callsign>Finish</callsign>
+      <name>Finish</name>
+      <model type="string">AI/HOMEMADE-timed-loop/targetmarker-inactive.xml</model>
+      <latitude type="double">%s</latitude>
+      <longitude type="double">%s</longitude>
+      <altitude type="double">%s</altitude>
+      <heading type="double">%s</heading>
+    </entry>' ;
+my $TPL_FINISH2 = '
+    <entry>
+      <name>Finish-Marker</name>
+      <model type="string">AI/HOMEMADE-timed-loop/finishmarker.xml</model>
+      <latitude type="double">%s</latitude>
+      <longitude type="double">%s</longitude>
+      <altitude type="double">%s</altitude>
+      <heading type="double">%s</heading>
+    </entry>
+' ;
+
+my $TPL_EXIT = '
+    <entry>
+      <name>Exit%02d</name>
+      <model type="string">AI/HOMEMADE-timed-loop/lineupmarker.xml</model>
+      <latitude type="double">%s</latitude>
+      <longitude type="double">%s</longitude>
+      <altitude type="double">%s</altitude>
+      <heading type="double">%s</heading>
+    </entry>
+' ;
+
+#    0: "disabled",
+#    1: "recording entrance",
+#    2: "recording loop",
+#    3: "recording exit",
+#    4: "dumped",
+
+#===============================================================================
+#                                                       VARIABLES INITIALISATION
+
+#===============================================================================
+#                                                                      FUNCTIONS
+sub main
+{
+    @out = ();
+
+    open(IN, $FILENAME_INC_HEADER_XML) or die ;
+    @header = <IN> ;
+    close(IN) ;
+    open(IN, $FILENAME_INC_FOOTER_XML) or die ;
+    @footer = <IN> ;
+    close(IN) ;
+
+    open(IN, $FILENAME_IN_CSV) or die ;
+    @data = <IN> ;
+    close(IN) ;
+
+    push @out, @header ;
+
+    $index_entrance = 0 ;
+    $index_loop = 0 ;
+    $index_exit = 0 ;
+    $previous_no_phase = 0 ;
+    for(@data)
+    {
+        chomp ;
+        ($no_phase, $lat, $lng, $alt, $hdg) = split(':') ;
+        #print join ' - ', ($no_phase, $lat, $lng, $alt, $hdg) ;
+
+        $hdg += 90 ; $hdg %= 360 ;
+
+        if($no_phase == 1)
+        {
+            push @out, sprintf($TPL_ENTRANCE, ++$index_entrance, $lat, $lng, $alt, $hdg) ;
+        }
+        elsif(($no_phase == 2) && ($previous_no_phase == 1))
+        {
+            push @out, sprintf($TPL_START1, $lat, $lng, $alt, $hdg) ;
+            push @out, sprintf($TPL_START2, $lat, $lng, $alt, $hdg) ;
+        }
+        elsif($no_phase == 2)
+        {
+            push @out, sprintf($TPL_LOOP, ++$index_loop, $lat, $lng, $alt, $hdg) ;
+        }
+        elsif(($no_phase == 3) && ($previous_no_phase == 2))
+        {
+            push @out, sprintf($TPL_FINISH1, $lat, $lng, $alt, $hdg) ;
+            push @out, sprintf($TPL_FINISH2, $lat, $lng, $alt, $hdg) ;
+        }
+        elsif($no_phase == 3)
+        {
+            push @out, sprintf($TPL_EXIT, ++$index_exit, $lat, $lng, $alt, $hdg) ;
+        }
+
+        $previous_no_phase = $no_phase ;
+    }
+
+    push @out, @footer ;
+
+    open(OUT, '>', $FILENAME_OUT_XML) or die ;
+    print OUT @out ;
+    close(OUT) ;
+
+}
+
+#===============================================================================
+#                                                                           MAIN
+
+main();
+
+### EOF
+
+
+
+
+
---
+++ tools/trace-loop/trace-loop.nas
@@ -0,0 +1,51 @@
+print("*** LOADING tool - trace-loop.nas ... ***");
+
+var no_phase = 0;
+var buffer_coords = [];
+var label_phase = {
+    0: "disabled",
+    1: "recording entrance",
+    2: "recording loop",
+    3: "recording exit",
+    4: "dumped",
+};
+
+# call from input keyboard (key 124)
+var event_reinit_loop = func() {
+    no_phase = 0;
+    buffer_coords = [];
+    printf("key pressed - reinit");
+}
+
+# call from input keyboard (key 92)
+var event_change_phase = func() {
+    no_phase += 1;
+    if(no_phase > 3)
+    {
+        foreach(e; buffer_coords)
+        {
+            print(e);
+        }
+        no_phase = 4;
+        #no_phase = 0;
+    }
+    printf ("key pressed - %s coords", label_phase[no_phase]);
+}
+
+var loop_get_coords = func() {
+    if((no_phase > 0) and (no_phase < 4))
+    {
+        var position = geo.aircraft_position();
+        
+        var lat = position.lat();
+        var lng = position.lon();
+        var alt = position.alt();
+        var hdg = getprop('/orientation/heading-deg') or 0;
+        
+        append(buffer_coords, sprintf("%s:%s:%s:%d:%d", no_phase, lat, lng, alt, hdg));
+    }
+    settimer(loop_get_coords, 6);
+}
+
+setlistener('/sim/signals/fdm-initialized', loop_get_coords);
+
